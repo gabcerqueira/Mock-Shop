ba982ed8b58795d235ff6fe6838af31799407fb4
gabcerqueira <43377719+gabcerqueira@users.noreply.github.com>
2020-10-08 09:32:47 -0300

    cart icon added with a cartdropdown

--- mock-shop-frontend/src/Components/navbar/Navbar.jsx
+++ mock-shop-frontend/src/Components/navbar/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../../Components/cart-icon/CartIcon";
+import CartDropdown from "../../Components/cart-dropdown/CartDropdown";
 import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase";
 import "./navbar.scss";
 
-const Navbar = ({ currentUser }) => {
+const Navbar = ({ currentUser, hidden }) => {
 	return (
 		<div className="navbar">
 			<Link to="/" className="content-logo">
@@ -34,13 +36,17 @@ const Navbar = ({ currentUser }) => {
 				<Link to="/contact" className="nav-list-item">
 					Contact
 				</Link>
+				<CartIcon />
 			</div>
+
+			{hidden ? null : <CartDropdown />}
 		</div>
 	);
 };
 
 const mapStateToProps = (state) => ({
 	currentUser: state.user.currentUser,
+	hidden: state.cart.hidden,
 });
 
 export default connect(mapStateToProps)(Navbar);
